f3c494927dbce6becb237caec87e6280793ddf41
heistifeh <teefeh26@gmail.com>
2025-05-16 15:32:37 +0100

    homepage complete

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-dark border-t border-gray-200 dark:border-gray-800">
+      <div className="container max-w-7xl mx-auto px-4 py-8">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:,b-0">
+            <Link href="/" className="text-xl font-bold text-primary">
+              TifeFolio&trade;
+            </Link>
+            <p>C {new Date().getFullYear()} TifeFolio. All rights reserved</p>
+          </div>
+          <div className="flex  space-x-6 ">
+            <Link
+              href={"https://github.com/heistifeh"}
+              target="_blank"
+              className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 transition-colors duration-300"
+            >
+              <FaGithub />
+            </Link>
+            <Link
+              href={"https://github.com/heistifeh"}
+              target="_blank"
+              className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 transition-colors duration-300"
+            >
+              <FaLinkedin />
+            </Link>
+            <Link
+              href={"https://github.com/heistifeh"}
+              target="_blank"
+              className="text-2xl text-gray-600 hover:text-primary dark:text-gray-300 transition-colors duration-300"
+            >
+              <FaTwitter />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
